11fabc3d283070b86ebc0d142f090856daf4f11c
Alex Hayes <al640711@dal.ca>
2025-01-21 08:18:24 -0400

    Added a Business Component. This component displays a photo of a Business as well as its adress and company name.

---
+++ src/Business.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+
+
+
+function Business() {
+
+    return (
+        <div>
+            <img src="" alt="Business Image"/>
+
+            <h2>Ravenous Pizza</h2>
+
+            <address>
+
+                <h4>123 Alex Drive</h4>
+                <p>Halifax</p>
+                <p>Nova Scotia</p>
+                <p>B11 212</p>
+                <p>Italian</p>
+                <p>5 stars</p>
+                <p>110 reviews</p> 
+            </address>
+        </div>
+    )
+}
+
+
+export default Business
